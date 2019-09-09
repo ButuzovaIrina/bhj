e7a3226e17c275a9b235f4c2dcd52a5d9bccd542
Irina Butuzova <butuzovairina18@gmail.com>
2019-09-09 13:25:12 +0300

    changed

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,13 +1,18 @@
 let winPop = document.getElementById("modal_main");
+let winSucc = document.getElementById("modal_success");
 winPop.className = "modal modal_active";
 
 let elementsClose = Array.from(document.getElementsByClassName("modal__close"));
 
 for ( i = 0; i < elementsClose.length; i++ ) {
-  elementsClose[i].onclick = () =>  winPop.className = "modal";
+  elementsClose[i].onclick = () =>  {
+    winPop.className = "modal";
+    winSucc.className = "modal";
+  }
 }
 
 let elementsSucces = document.getElementsByClassName("show-success");
-elementsSucces[0].onclick = () => 
-  document.getElementById("modal_success").className = "modal modal_active";
-
+elementsSucces[0].onclick = () => {
+  winPop.className = "modal";
+  winSucc.className = "modal modal_active";
+}
