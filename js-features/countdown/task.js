7d0cda39d42151b46b8bce15c8c14b84e3252871
Irina Butuzova <butuzovairina18@gmail.com>
2019-09-09 13:20:43 +0300

    changed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,6 +4,7 @@ let stop = timer.textContent * 1000;
 let addTimer = setInterval(() => {
   if (timer.textContent == 0) {
     alert("Вы победили в конкурсе"); 
+    timer.textContent = 5;
   } else {
     timer.textContent -= 1;
   }
