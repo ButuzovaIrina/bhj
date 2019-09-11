e68d3c7f94c0b33fb27ed3bf4d9a631699fbcec5
Irina Butuzova <butuzovairina18@gmail.com>
2019-09-11 14:30:50 +0300

    first commit book-reader

---
+++ dom/book-reader/task.js
@@ -0,0 +1,24 @@
+let arraySize = Array.from(document.querySelectorAll(".font-size"));
+
+
+function changeSize() {   
+  for (let i = 0; i < arraySize.length; i++) {
+    if (arraySize[i].classList.contains("font-size_active")) {
+      arraySize[i].classList.remove("font-size_active"); 
+    }
+  }
+  this.classList.add("font-size_active"); 
+    document.getElementById("book").className = "book"
+  if (this.classList.contains("font-size_small")){
+    document.getElementById("book").classList.add("book_fs-small");
+  }
+  if (this.classList.contains("font-size_big")){
+    document.getElementById("book").classList.add("book_fs-big");
+  }
+
+}
+
+for (let size of arraySize) {
+    size.addEventListener("click", changeSize);
+  }
+  
