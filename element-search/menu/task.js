366aae565aee2e9fab28431105d61cb942daaa78
Irina Butuzova <butuzovairina18@gmail.com>
2019-09-11 13:36:16 +0300

    menu is changed

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,11 +4,16 @@ console.log(elementList);
 
 function addSelectedClassIfClick() {
   if (this.nextElementSibling.classList.contains("menu_sub")) {
-    if (document.querySelector("ul.menu.menu_sub.menu_active") !== null) {
-      document.querySelector("ul.menu.menu_sub.menu_active").classList.toggle("menu_active");
-    }
-    event.preventDefault();
-    this.nextElementSibling.classList.toggle("menu_active");
+    if (this.nextElementSibling.classList.contains("menu_active")) {
+      this.nextElementSibling.classList.remove("menu_active");
+    } else {
+      if (document.querySelector("ul.menu.menu_sub.menu_active") !== null) {
+        document.querySelector("ul.menu.menu_sub.menu_active").classList.remove("menu_active");
+      }  
+      event.preventDefault();  
+      this.nextElementSibling.classList.add("menu_active");
+    } 
+
   } 
 }
 
