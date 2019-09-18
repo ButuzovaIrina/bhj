5cbedf94914483f1ae03c4ec9635ad14e561d031
Irina Butuzova <butuzovairina18@gmail.com>
2019-09-18 17:21:41 +0300

    changed book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -8,14 +8,15 @@ function changeSize() {   
     }
   }
   this.classList.add("font-size_active"); 
-    document.getElementById("book").className = "book"
+  document.getElementById("book").className = "book";
+  
   if (this.classList.contains("font-size_small")){
     document.getElementById("book").classList.add("book_fs-small");
   }
   if (this.classList.contains("font-size_big")){
     document.getElementById("book").classList.add("book_fs-big");
   }
-
+  event.preventDefault();
 }
 
 for (let size of arraySize) {
